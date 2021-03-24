e4364972c63614e029498e2568640eb892f3c09c
Michal <michal.p.zarzycki@wp.pl>
2021-03-24 18:33:06 +0100

    Set up server

---
+++ index.js
@@ -0,0 +1,8 @@
+const http = require('http')
+const PORT = process.env.PORT || 80;
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.write('Hello World!');
+    res.end();
+  }).listen(PORT);
